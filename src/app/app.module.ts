17a9ed4cecfa2198d59b81e3ee006b937ae6cd49
kumar <santanupaikaray1996@gmail.com>
2021-04-04 03:48:31 +0530

    add all files

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {CoursesModule} from './courses/courses.module';
+import {StaticpagesModule} from './staticpages/staticpages.module';
+import {AdminModule} from './admin/admin.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { from } from 'rxjs';
+import { AuthModule } from './auth/auth.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoursesModule,
+    StaticpagesModule,
+    AuthModule,
+    AdminModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
