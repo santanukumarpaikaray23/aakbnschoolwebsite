4d86f4e051f4f36e5bcc3699ff2af2df30c71af1
kumar <santanupaikaray1996@gmail.com>
2021-04-15 02:39:06 +0530

    add all changes

--- src/app/courses/course-featured/course-featured.component.ts
+++ src/app/courses/course-featured/course-featured.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-featured.component.css']
 })
 export class CourseFeaturedComponent implements OnInit {
+  
 
   constructor() { }
 
